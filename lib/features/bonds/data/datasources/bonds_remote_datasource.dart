a41a48856fb07ee89c125237bef3a4e623fcd49a
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2025-07-10 12:25:59 +0530

    Add BondsRemoteDatasource with Dio implementation

---
+++ lib/features/bonds/data/datasources/bonds_remote_datasource.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import '../models/bond_model.dart';
+
+abstract class BondsRemoteDatasource {
+  Future<List<BondModel>> getBonds();
+  Future<BondModel> getBondDetail(String isin);
+}
+
+class BondsRemoteDatasourceImpl implements BondsRemoteDatasource {
+  final Dio dio;
+
+  BondsRemoteDatasourceImpl(this.dio);
+
+  @override
+  Future<List<BondModel>> getBonds() async {
+    final response = await dio.get('https://eol122duf9sy4de.m.pipedream.net');
+    final data = List<Map<String, dynamic>>.from(response.data);
+    return data.map((json) => BondModel.fromJson(json)).toList();
+  }
+
+  @override
+  Future<BondModel> getBondDetail(String isin) async {
+    final response = await dio.get(
+      'https://eo61q3zd4heiwke.m.pipedream.net',
+      queryParameters: {'id': isin},
+    );
+    return BondModel.fromJson(response.data);
+  }
+}
