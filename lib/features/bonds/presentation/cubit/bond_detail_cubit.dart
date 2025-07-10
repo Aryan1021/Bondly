db23f204843aab39dfe307b5ed9e5c3d496c1f7b
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2025-07-10 23:31:37 +0530

    Refactor BondDetailCubit to use GetBondDetail use case

--- lib/features/bonds/presentation/cubit/bond_detail_cubit.dart
+++ lib/features/bonds/presentation/cubit/bond_detail_cubit.dart
@@ -1,18 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
-import 'package:bondly/features/bonds/domain/repository/bond_repository.dart';
+import 'package:bondly/features/bonds/domain/usecases/get_bond_detail.dart';
 import 'bond_detail_state.dart';
 
 @injectable
 class BondDetailCubit extends Cubit<BondDetailState> {
-  final BondRepository repository;
+  final GetBondDetail getBondDetail;
 
-  BondDetailCubit(this.repository) : super(BondDetailInitial());
+  BondDetailCubit(this.getBondDetail) : super(BondDetailInitial());
 
   Future<void> fetchBondDetail(String isin) async {
     emit(BondDetailLoading());
     try {
-      final bond = await repository.getBondDetail(isin);
+      final bond = await getBondDetail(isin);
       emit(BondDetailLoaded(bond));
     } catch (e) {
       emit(BondDetailError('Failed to load bond detail: ${e.toString()}'));
